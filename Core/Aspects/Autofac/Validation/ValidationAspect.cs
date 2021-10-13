9c20f0982bd2614660f729c3e4eee1f0889c5fb9
SevtapE <sevtaperbek@outlook.com>
2021-10-13 07:00:10 +0300

    Developed an interception infrastructure by using AOP techniques. Added a validation aspect. Interception infrastructure is included by configuring on the Autofac module.

---
+++ Core/Aspects/Autofac/Validation/ValidationAspect.cs
@@ -0,0 +1,39 @@
+﻿using Castle.DynamicProxy;
+using Core.CrossCuttingConcerns.Validation.FluentValidation;
+using Core.Utilities.Interceptors;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Aspects.Autofac.Validation
+{
+    public class ValidationAspect:MethodInterception
+    {
+        private Type _validatorType;
+
+        public ValidationAspect(Type validatorType)
+        {
+            if (!validatorType.IsAssignableFrom(typeof(IValidator)))
+                {
+                throw new Exception("It is not a validator.");
+            }
+            _validatorType = validatorType;
+        }
+
+        protected override void OnBefore(IInvocation invocation)
+        {
+          
+          var validator=(IValidator)Activator.CreateInstance(_validatorType);
+            var entityType = _validatorType.BaseType.GetGenericArguments()[0];
+            var entitiesOfMethod = invocation.Method.GetParameters();
+
+            foreach (var entity in entitiesOfMethod)
+            {
+                ValidationTool.Validate(validator, entity);
+            }
+        }
+    }
+}
